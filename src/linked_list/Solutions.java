18e28fe45a5313c4a7b1321a57da1451ab2e3237
Eric Pan <ericpan0207@gmail.com>
2019-09-10 21:49:50 -0700

    java solutions file

---
+++ src/linked_list/Solutions.java
@@ -0,0 +1,9 @@
+class Solutions {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
