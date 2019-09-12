c1bbb454a5d6edb3a2177439029eb5242b367622
Eric Pan <ericpan0207@gmail.com>
2019-09-12 04:37:39 -0700

    Added linkedlist creation through list, and print method

--- src/linked_list/Solutions.java
+++ src/linked_list/Solutions.java
@@ -1,3 +1,6 @@
+import static java.util.Arrays.asList;
+import java.util.List;
+import java.util.Arrays;
 class Solutions {
     /* Tortoise and hare pointers:
      *  Original idea: Find length of list and subtract n to find location to skip
@@ -7,7 +10,7 @@ class Solutions {
      */
     // Example : 1 -> 2 -> 3 -> 4 -> 5, n = 2
     //  return : 1 -> 2 -> 3 -> 5
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode slow = head, fast = head;
         // Move fast in front so the gap between slow and fast becomes n
         for (int i = 0; i < n; i++) {
@@ -33,7 +36,7 @@ class Solutions {
      */
     // Example: 1 -> 2 -> 3 -> 4 -> 5
     //  change: 1 -> 5 -> 2 -> 4 -> 3
-    public void reorderList(ListNode head) {
+    public static void reorderList(ListNode head) {
         if (head == null || head.next == null)
             return;
         // Step 1. cut list to two halves
@@ -55,7 +58,8 @@ class Solutions {
         merge(l1, l2);
     }
 
-    private ListNode reverse(ListNode head) {
+    // Modifies original head
+    private static ListNode reverse(ListNode head) {
         ListNode prev = null, curr = head, next = null;
 
         while (curr != null) {
@@ -68,12 +72,36 @@ class Solutions {
     }
 
     // Merges two linkedlists in alternating order
-    private void merge(ListNode l1, ListNode l2) {
+    private static void merge(ListNode l1, ListNode l2) {
         while (l2 != null) {
             ListNode next = l1.next;
             l1.next = l2;
             l1 = l1.next;
             l2 = next;
         }
+    }
+
+    private static ListNode linkedList(List<Integer> list) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+        for (int num : list) {
+            curr.next = new ListNode(num);
+            curr = curr.next;
+        }
+        return head.next;
+    }
+
+    private static void print(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + ", ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4);
+        ListNode test = linkedList(list);
+        print(test);
     }
 }
