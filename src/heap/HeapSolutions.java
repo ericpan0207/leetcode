fbca6529943e87c029dd8315689c67d8462298b3
Eric Pan <ericpan0207@gmail.com>
2019-09-15 02:15:08 -0700

    topKFreq java

--- src/heap/HeapSolutions.java
+++ src/heap/HeapSolutions.java
@@ -1,11 +1,15 @@
-import static java.util.Arrays.asList;
-import java.util.List;
-import java.util.Arrays;
+import java.util.*;
 
 class HeapSolutions {
     // Sorting multiple lists that are already sorted:
     // Place everything into heap O(log n) * O(n) for n num of elements
     // Remove everything from heap O(log n) * O(n) for n num of elements
+    //
+    // Example: [[1, 4, 5],
+    //           [1, 3, 4],
+    //           [2, 6]
+    //          ]
+    // returns: [1, 1, 2, 3, 4, 4, 5, 6]
     public static ListNode mergeKLists(ListNode[] lists) {
         Heap minHeap = new Heap();
         for (ListNode n : lists) {
@@ -26,17 +30,48 @@ class HeapSolutions {
         return head;
     }
 
+    // Original Idea: Map and heap, build maxheap and pop k times
+    //   Better Idea: build minHeap and remove when > k, this ensures small heap
+    //  Also, one line for map insert
+    //
+    //  Example: [1, 1, 1, 2, 2, 3], k = 2
+    //   Output: [1, 2]
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        // Sort by least freq first
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((n1, n2) -> (freq.get(n1) - freq.get(n2)));
+        for (int num : freq.keySet()) {
+            heap.add(num);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+        List<Integer> topK = new ArrayList<Integer>();
+        while (!heap.isEmpty()) {
+            topK.add(heap.poll());
+        }
+        Collections.reverse(topK);
+        return topK;
+    }
+
     public static void main(String[] args) {
-        List<Integer> list1 = Arrays.asList(1, 4, 5);
-        List<Integer> list2 = Arrays.asList(1, 3, 4);
-        List<Integer> list3 = Arrays.asList(2, 6);
-        ListNode n1 = LinkedListSolutions.linkedList(list1);
-        ListNode n2 = LinkedListSolutions.linkedList(list2);
-        ListNode n3 = LinkedListSolutions.linkedList(list3);
-        ListNode[] lists = {n1, n2, n3};
+        // Testing mergeKLists
+        // ___________________
+        // List<Integer> list1 = Arrays.asList(1, 4, 5);
+        // List<Integer> list2 = Arrays.asList(1, 3, 4);
+        // List<Integer> list3 = Arrays.asList(2, 6);
+        // ListNode n1 = LinkedListSolutions.linkedList(list1);
+        // ListNode n2 = LinkedListSolutions.linkedList(list2);
+        // ListNode n3 = LinkedListSolutions.linkedList(list3);
+        // ListNode[] lists = {n1, n2, n3};
 
-        ListNode result = mergeKLists(lists);
-        LinkedListSolutions.print(result);
-
+        // ListNode result = mergeKLists(lists);
+        // LinkedListSolutions.print(result);
+        //
+        int[] nums = new int[] {1, 1, 1, 2, 2, 3};
+        System.out.println(topKFrequent(nums, 2));
     }
 }
