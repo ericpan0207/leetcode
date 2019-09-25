0d82708947b0f1ffa4adfb47d956d1ef8a39b172
Eric Pan <ericpan0207@gmail.com>
2019-09-25 03:44:48 -0700

    Starting trie data structure

---
+++ src/trie/Trie.java
@@ -0,0 +1,11 @@
+public class Trie {
+    private TrieNode root;
+
+    public void insert(String word) {
+        TrieNode current = root;
+
+        for (int i = 0; i < word.length()); ++i {
+            current = 
+        }
+    }
+}
---
+++ src/trie/TrieNode.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class TrieNode {
+    private HashMap<Character, TrieNode> children;
+    private String content;
+    private boolean isWord;
+}
